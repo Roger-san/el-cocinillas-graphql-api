c585db5d826190c68ca539ec88783f30f8c1db64
Roger <72167137+Roger-san@users.noreply.github.com>
2022-12-26 12:47:01 +0100

    Netifly changes

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const server = new ApolloServer({
   playground: true
 })
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`Server ready at: ${url}`)
-})
+server.listen({ port: process.env.PORT || 4000 })
+  // .then(({ url }) => {
+  // console.log(`Server ready at: ${url}`)
+// })
